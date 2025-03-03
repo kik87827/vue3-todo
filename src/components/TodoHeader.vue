6f2998bade5522976550cd5f5de8a3b772af32e4
kik8727 <kik87827@gmail.com>
2025-03-03 23:32:11 +0900

    0303 진행중중

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,3 +1,24 @@
 <template>
-  <div><h1>TODO 앱</h1></div>
+  <div>
+    <h1>{{ appTitle }}</h1>
+    <h4>{{ newTitle }}</h4>
+  </div>
 </template>
+
+<script>
+import { computed } from 'vue';
+
+export default {
+  props: {
+      appTitle : String
+  },
+  setup(props) {
+    const newTitle = computed(() => {
+      return props.appTitle + "!!";
+    })
+
+    return {newTitle}
+  }
+}
+  
+</script>
